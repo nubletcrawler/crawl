2f58f8899bc18312364780d9631d90eef98d7cbb
nubletcrawler <nubsquishynub@gmail.com>
2018-02-28 21:52:23 +0100

    Just removed a "fixme test" reminder.

--- crawl-ref/source/spl-zap.cc
+++ crawl-ref/source/spl-zap.cc
@@ -88,7 +88,7 @@ static pair<spell_type, zap_type> _spl_zaps[] =
     { SPELL_ACID_SPLASH, ZAP_BREATHE_ACID },
     { SPELL_BORGNJORS_VILE_CLUTCH, ZAP_VILE_CLUTCH},
 	{ SPELL_HEAL_WOUNDS, ZAP_HEAL_WOUNDS },
-	{ SPELL_TELEPORT, ZAP_TELEPORT_OTHER },	//FIXME test
+	{ SPELL_TELEPORT, ZAP_TELEPORT_OTHER },	//functionally the same?
 
 };
 
